317c9e3a54fc23839567bfdfc9e432df44639dc7
Калёнов Денис Михайлович <a-KaDeMi-k@yandex.ru>
2015-04-06 11:16:12 +0300

    Aeschenbacher

--- tests/phpunit/tests/class.gb-transcriptor/de.php
+++ tests/phpunit/tests/class.gb-transcriptor/de.php
@@ -8,7 +8,7 @@ class Tests_Transcriptor_DE extends GB_UnitTestCase {
 		$data = array(
 				'ru' => array(
 					'Aachen'		=> 'Ахен',				'Achslach'		=> 'Акслах',
-					'Adenauer'		=> 'Аденауэр',			'Ae sche nbacher'	=> 'Э ше нбахер',
+					'Adenauer'		=> 'Аденауэр',			'Aeschenbacher'	=> 'Эшенбахер',
 					'Ahrenviöl'		=> 'Аренфиёль',			'Aichinger'		=> 'Айхингер',
 					'Altjürden'		=> 'Альтйюрден',		'Altschul'		=> 'Альтшуль',
 					'Andreas'		=> 'Андреас',			'Anjun'			=> 'Аньюн',
